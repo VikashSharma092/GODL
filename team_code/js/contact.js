66d5ecc223f2ae6fd4ea62871eac51fe337e0235
Adit <adityaop933@gmail.com>
2026-08-17 16:52:04 +0530

    4th

--- team_code/js/contact.js
+++ team_code/js/contact.js
@@ -1,5 +1,8 @@
 //COMMIT 1 START: 
-
+// Contact page initialization & logger
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("📧 Contact Page JS successfully loaded!");
+});
 //COMMIT 1 END: 
 
 
